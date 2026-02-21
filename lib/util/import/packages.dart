7d9c71030fc0a248e8748de2899027c26c1bf32e
Gino Laranjeiras <gino@cutia.tech>
2026-02-21 20:31:37 -0300

    chore: remove extra blank line and add flutter_localizations export
    Remove an unnecessary blank line in pubspec.yaml dependencies section for cleaner formatting. Also add missing export for flutter_localizations package to packages.dart file to ensure all required package exports are available.

--- lib/util/import/packages.dart
+++ lib/util/import/packages.dart
@@ -5,3 +5,5 @@ export 'package:go_router/go_router.dart';
 export 'package:get_it/get_it.dart';
 
 export 'package:shared_preferences/shared_preferences.dart';
+
+export 'package:flutter_localizations/flutter_localizations.dart';
